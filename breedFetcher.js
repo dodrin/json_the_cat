73fda04cf49cf0913ab30e9be0c507f9d9a7f85a
Rina Inada <indrn@icloud.com>
2023-06-19 17:27:13 +0000

    refactor breed fetcher: change else if to return

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,25 @@
 const request = require("request");
 
-const fetchBreedDescription = function(breedName, callback) {
-
+const fetchBreedDescription = function (breedName, callback) {
   const api = "https://api.thecatapi.com/v1/breeds/search?q=";
   const url = api + breedName;
 
-  request(url, function(error, response, body) {
+  request(url, function (error, response, body) {
     const data = JSON.parse(body);
-    
+
     if (error) {
       callback(error, null);
-    } else if (breedName === undefined || data[0] === undefined) {
+      return;
+    }
+
+    if (breedName === undefined || data[0] === undefined) {
       error = "Your request breed does not exist";
       callback(error, null);
-    } else {
-      let desc = data[0].description;
-      callback(null, desc);
+      return;
     }
+
+    let desc = data[0].description;
+    callback(null, desc);
   });
 };
 
